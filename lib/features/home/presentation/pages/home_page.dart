a8904a6e977af901f34a304fc2d4fc46fecf0f41
Taisei Nagata <nagacotaco@gmail.com>
2026-03-21 15:30:00 +0900

    feat: add home page view model and extract body into a private widget.

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -2,22 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../application/view_model/home_page_view_model.dart';
+
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(homePageVMProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('ホーム画面'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.push('/detail');
-          },
-          child: const Text('詳細画面へ遷移'),
-        ),
+      body: _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          context.push('/detail');
+        },
+        child: const Text('詳細画面へ遷移'),
       ),
     );
   }
